0616ff81f87f2ddddd4428205395ff75b105b01b
Julian Merkle <juli.merkle@gmail.com>
2021-05-29 14:21:08 +0200

    Use kelseyhightower/envconfig for the cfg

--- app/cfg/cfg.go
+++ app/cfg/cfg.go
@@ -2,21 +2,14 @@ package cfg
 
 import (
 	"errors"
-	"os"
-	"strconv"
+	"strings"
 
-	"github.com/joho/godotenv"
-	log "github.com/sirupsen/logrus"
+	"github.com/kelseyhightower/envconfig"
 )
 
 var (
-	ErrLinkerDefsMissing    = errors.New("link time definitions missing (GitHash/Buildtime)")
-	ErrBadLogLevel          = errors.New("LOGLEVEL must be an integer")
-	ErrBadHTTPPort          = errors.New("HTTP_PORT must be an integer")
-	ErrBadBaseHREF          = errors.New("BASE_HREF must begin and end with a slash")
-	ErrBadGemDefaultPort    = errors.New("GEM_DEFAULT_PORT must be an integer")
-	ErrBadGemRespMemLimit   = errors.New("GEM_RESP_MEM_LIMIT must be an integer")
-	ErrBadGemRedirectsLimit = errors.New("GEM_MAX_REDIRECTS must be an integer")
+	ErrLinkerDefsMissing = errors.New("link time definitions missing (GitHash/Buildtime)")
+	ErrBadBaseHREF       = errors.New("BASE_HREF must begin and end with a slash")
 )
 
 // Injected with linker flags
@@ -26,85 +19,35 @@ var (
 )
 
 type Cfg struct {
-	AppVersion        string // Application version (e.g. 1.13.5)
-	AppBuildMeta      string // Application build meta information (e.g. ae5f03-2021)
-	LogLevel          int    // sirupsen/logrus log level
-	HTTPPort          string // HTTP server port
-	BaseHREF          string // Base HREF for the application (e.g. / or /gemportal/)
-	GemDefaultPort    string // Default Gemini port
-	GemRespMemLimit   int64  // Gemini response limit in bytes
-	GemRedirectsLimit uint32 // Maximum gemini redirects to follow
+	AppVersion        string `ignored:"true"`                                    // Application version (e.g. 1.13.5)
+	AppBuildMeta      string `ignored:"true"`                                    // Application build meta information (e.g. ae5f03-2021)
+	LogLevel          int    `envconfig:"LOGLEVEL" default:"4"`                  // sirupsen/logrus log level (log.InfoLevel)
+	HTTPPort          string `envconfig:"HTTP_PORT" default:"8080"`              // HTTP server port
+	BaseHREF          string `envconfig:"BASE_HREF" default:"/"`                 // Base HREF for the application (e.g. / or /gemportal/)
+	GemDefaultPort    string `envconfig:"GEM_DEFAULT_PORT" default:"1965"`       // Default Gemini port
+	GemRespMemLimit   int64  `envconfig:"GEM_RESP_MEM_LIMIT" default:"31457280"` // Gemini response limit in bytes (30MiB)
+	GemRedirectsLimit uint32 `envconfig:"GEM_MAX_REDIRECTS" default:"3"`         // Maximum gemini redirects to follow
 }
 
 // GetConfig loads and checks the application configuration
 func GetConfig(appVersion string) (*Cfg, error) {
 	var cfg Cfg
 
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		return nil, err
+	}
+
 	if len(gitHash) == 0 || len(buildTime) == 0 {
 		return nil, ErrLinkerDefsMissing
 	}
-
 	cfg.AppVersion = appVersion
 	cfg.AppBuildMeta = gitHash + "-" + buildTime
 
-	_ = godotenv.Load()
-
-	if logLevel := os.Getenv("LOGLEVEL"); len(logLevel) == 0 {
-		cfg.LogLevel = int(log.InfoLevel)
-	} else if logLevelInt, err := strconv.Atoi(logLevel); err != nil {
-		return nil, ErrBadLogLevel
-	} else {
-		cfg.LogLevel = logLevelInt
-	}
-
-	if httpPort := os.Getenv("HTTP_PORT"); len(httpPort) == 0 {
-		cfg.HTTPPort = "8080"
-	} else if !isInteger(httpPort) {
-		return nil, ErrBadHTTPPort
-	} else {
-		cfg.HTTPPort = httpPort
-	}
-
-	if baseHREF := os.Getenv("BASE_HREF"); len(baseHREF) == 0 {
-		cfg.BaseHREF = "/"
-	} else if baseHREF[0] != '/' || baseHREF[len(baseHREF)-1] != '/' {
+	// The base HREF has to be pre- and suffixed by a slash
+	if !strings.HasPrefix(cfg.BaseHREF, "/") || !strings.HasSuffix(cfg.BaseHREF, "/") {
 		return nil, ErrBadBaseHREF
-	} else {
-		cfg.BaseHREF = baseHREF
-	}
-
-	if gemDefaultPort := os.Getenv("GEM_DEFAULT_PORT"); len(gemDefaultPort) == 0 {
-		cfg.GemDefaultPort = "1965"
-	} else if !isInteger(gemDefaultPort) {
-		return nil, ErrBadGemDefaultPort
-	} else {
-		cfg.GemDefaultPort = gemDefaultPort
-	}
-
-	if value := os.Getenv("GEM_RESP_MEM_LIMIT"); len(value) == 0 {
-		cfg.GemRespMemLimit = int64(30 * 1024 * 1024) // 30 MiB
-	} else if num, err := strconv.ParseInt(value, 10, 64); err != nil {
-		return nil, ErrBadGemRespMemLimit
-	} else {
-		cfg.GemRespMemLimit = num
-	}
-
-	if value := os.Getenv("GEM_MAX_REDIRECTS"); len(value) == 0 {
-		cfg.GemRedirectsLimit = 3
-	} else if num, err := strconv.ParseUint(value, 10, 32); err != nil {
-		return nil, ErrBadGemRedirectsLimit
-	} else {
-		cfg.GemRedirectsLimit = uint32(num)
 	}
 
 	return &cfg, nil
-}
-
-// isInteger checks if a string is an integer
-func isInteger(s string) bool {
-	if _, err := strconv.Atoi(s); err != nil {
-		return false
-	} else {
-		return true
-	}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	code.rocketnine.space/tslocum/gmitohtml v1.0.4
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/makeworld-the-better-one/go-gemini v0.11.1
 	github.com/microcosm-cc/bluemonday v1.0.9
 	github.com/patrickmn/go-cache v2.1.0+incompatible
