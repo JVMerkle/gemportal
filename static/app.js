517a542b98b38affa45009d6b2cd07ab752a31be
Julian Merkle <me@jvmerkle.de>
2022-03-25 19:04:45 +0100

    Add SPDX license identifiers to the project

--- static/app.js
+++ static/app.js
@@ -1,3 +1,6 @@
+/* SPDX-FileCopyrightText: 2021-2022 Contributors of gemportal */
+/* SPDX-License-Identifier: AGPL-3.0-only */
+
 function onSubmit() {
     let url = document.getElementById("idUrl");
 
